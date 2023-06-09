a153740d029d0333b620dd8b06db12569c561d29
Chamith-Digitus <chamith.h@digitustecglobal.com>
2023-06-09 23:01:04 +0530

    week 6

---
+++ src/Request.jsx
@@ -0,0 +1,17 @@
+// import { Axios } from "axios";     ----> Error line previous
+import Axios from "axios";
+import { Component } from "react";
+
+class Request extends Component {
+    serverURL = "http://127.0.0.1:8000"
+
+    post_ActionMyData(data) {
+        const route = "/user"
+        const path = "/posting"
+
+        return Axios.post(this.serverURL + route + path, data)
+    }
+
+}
+
+export default Request
--- src/Website/Pages/Home.jsx
+++ src/Website/Pages/Home.jsx
@@ -1,7 +1,44 @@
+import Request from "../../Request"
+
 function Home() {
+    const haddle_myData =async()=> {
+        // try {
+        //     const mydata = await Axios.get("http://127.0.0.1:8000/user/userobject")
+        //     console.log(mydata)
+        // }
+
+        // catch(err) {
+        //     console.log(err)
+        // }
+    }
+
+    const posting_myData =async()=> {
+        
+        try {
+
+            const dataset = {
+                name:'Chamith',
+                industry:'SE'
+            }
+    
+            const request = new Request
+            const response = await request.post_ActionMyData(dataset)
+            console.log(response)
+            
+        }
+
+        catch(error) {
+            console.log(error)
+        }
+    }
+
+
     return (
         <div className="Home">
             <h1>Home is working</h1>
+
+            <button onClick={haddle_myData}>Get my data</button>
+            <button onClick={posting_myData}>Post My data</button>
         </div>
     )
 }
